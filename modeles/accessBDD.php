d0436ec861906056832198453192513098e94e93
tommy silva <tommy.47@hotmail.com>
2019-01-05 18:20:40 +0100

    [T2-Tommy] Ajout fonction de connection PDO classe accessBDD

--- modeles/accessBDD.php
+++ modeles/accessBDD.php
@@ -1,3 +1,23 @@
 <?php
 
+$serverName = "localhost";
+$username = "username";
+$password = "password";
+$dbName = "dbName";
+
+function getConnection()
+{
+    global $serverName, $username, $password, $dbName;
+
+    echo 'Tentative de connexion au serveur : '. $serverName;
+    try {
+        $conn = new PDO('mysql:host=' . $serverName . 'dbname=' . $dbName, $username, $password);
+    } catch (PDOException $e) {
+        $msg = 'Erreur PDO : impossible de se connecter ';
+        die($msg);
+    }
+
+    return $conn;
+}
+
 ?>
