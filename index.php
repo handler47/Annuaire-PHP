6c1019240beed70b9ce6f070390f5e2481a97a35
Mariesc <31404263+Mariesc@users.noreply.github.com>
2019-01-05 12:00:12 +0100

    Ajout fichiers pour index

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+//header
+ require_once 'vues/header.php';
+
+//référencer les classes utiles
+require_once 'modeles/accessBDD.php';
+
+//connexion à la BDD
+
+
+//Controleur
+ require_once 'controleurs/controleurPrincipal.php';
+
+//footer
+require_once 'vues/footer.php';
+?>
