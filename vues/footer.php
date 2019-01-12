8e155e5cceba0f61ef3d9dad2172a57f1488f3c8
Mariesc <31404263+Mariesc@users.noreply.github.com>
2019-01-12 18:30:34 +0100

    Mise à jour CSS

--- vues/footer.php
+++ vues/footer.php
@@ -2,14 +2,15 @@
 
 ?>
 <center>
-	<footer id="footer">
+	<footer class="footer">
 		<div class="top-footer">
-			<div>
-				<a id="lien" href="index.php">Accueil</a>
+			<div class="lienretour">
+				<a class="lien" href="index.php">Accueil</a>
 			</div>
 			<div>
 				<p>2018 Annuaire Equipe : Marie SAPHAR CADET & Tommy SILVA	</p>
-			</div>	
+			</div>
+		</div>
 	</footer>
 </center>
 </body>
