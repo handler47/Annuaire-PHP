0048faaba2aac487c3fed0f6f7ad99c84ed7c255
Mariesc <31404263+Mariesc@users.noreply.github.com>
2019-01-12 18:33:15 +0100

    Modif meta du header

--- vues/header.php
+++ vues/header.php
@@ -5,15 +5,15 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta http-equiv="Content-Type" content="text/html">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Annuaire PHP</title>
 	<link href="css/MyCSS.css" rel="stylesheet">
 </head>
 	<body>
 		<center>
-		<div class="well center-block" id="title">
-			<h1>Annuaire de Contact <img src="img/iut.png" alt="img/iut.png" class="img-thumbnail"></h1>
+		<div class="blocktitre" id="title">
+			<h1>Annuaire de Contact <img src="img/iut.png" alt="img/iut.png" class="imgTitre"></h1>
 		</div>
 		</center>
 	
