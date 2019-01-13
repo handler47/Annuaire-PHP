ab0050b73b3d3585c6fdfc6b0cdcd3c97c884df7
tommy silva <tommy.47@hotmail.com>
2019-01-13 21:01:28 +0100

    [T0-Tommy] Ajout getter setters classes Contact et Addresse/renvoi d'addresse in-line

--- modeles/Adresse.php
+++ modeles/Adresse.php
@@ -9,16 +9,105 @@
 
 
 class Adresse {
+    private $ville;
     private $numRue;
     private $nomRue;
     private $codePostal;
     private $pays;
 
-    public function __construct($pNumRue, $pNomRue, $pCodePostal, $pPays)
+    public function __construct($pVille, $pNumRue, $pNomRue, $pCodePostal, $pPays)
     {
-        $this->numRue = $pNomRue;
+        $this->ville = $pVille;
+        $this->numRue = $pNumRue;
         $this->nomRue = $pNomRue;
         $this->codePostal= $pCodePostal;
         $this->pays = $pPays;
+    }
+    /**
+     * @return mixed
+     */
+    public function getVille()
+    {
+        return $this->ville;
+    }
+
+    /**
+     * @param mixed $ville
+     */
+    public function setVille($ville)
+    {
+        $this->ville = $ville;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNumRue()
+    {
+        return $this->numRue;
+    }
+
+    /**
+     * @param mixed $numRue
+     */
+    public function setNumRue($numRue)
+    {
+        $this->numRue = $numRue;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNomRue()
+    {
+        return $this->nomRue;
+    }
+
+    /**
+     * @param mixed $nomRue
+     */
+    public function setNomRue($nomRue)
+    {
+        $this->nomRue = $nomRue;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCodePostal()
+    {
+        return $this->codePostal;
+    }
+
+    /**
+     * @param mixed $codePostal
+     */
+    public function setCodePostal($codePostal)
+    {
+        $this->codePostal = $codePostal;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPays()
+    {
+        return $this->pays;
+    }
+
+    /**
+     * @param mixed $pays
+     */
+    public function setPays($pays)
+    {
+        $this->pays = $pays;
+    }
+
+    /**
+     * Renvoie l'adresse sur une seule ligne.
+     * @return string adresse
+     */
+    public function getAdresseInLine(){
+        return $this->numRue . ' ' . $this->nomRue + ' ' . $this->codePostal . ' ' . $this->ville . ' ' + $this->pays;
     }
 }
--- modeles/Contact.php
+++ modeles/Contact.php
@@ -16,13 +16,110 @@ class Contact {
     private $dateNaissance;
     private $commentaire;
 
-public function __construct($pNom, $pPrenom, $pSociete, $pAdresse, $pDateNaissance, $pCommentaire)
-{
-    $this->nom = $pNom;
-    $this->prenom = $pPrenom;
-    $this->societe = $pSociete;
-    $this->adresse = $pAdresse;
-    $this->dateNaissance = $pDateNaissance;
-    $this->commentaire = $pCommentaire;
-}
+    public function __construct($pNom, $pPrenom, $pSociete, $pAdresse, $pDateNaissance, $pCommentaire)
+    {
+        $this->nom = $pNom;
+        $this->prenom = $pPrenom;
+        $this->societe = $pSociete;
+        $this->adresse = $pAdresse;
+        $this->dateNaissance = $pDateNaissance;
+        $this->commentaire = $pCommentaire;
+    }
+    
+    /**
+     * @return mixed
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @param mixed $nom
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * @param mixed $prenom
+     */
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSociete()
+    {
+        return $this->societe;
+    }
+
+    /**
+     * @param mixed $societe
+     */
+    public function setSociete($societe)
+    {
+        $this->societe = $societe;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAdresse()
+    {
+        return $this->adresse;
+    }
+
+    /**
+     * @param mixed $adresse
+     */
+    public function setAdresse($adresse)
+    {
+        $this->adresse = $adresse;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDateNaissance()
+    {
+        return $this->dateNaissance;
+    }
+
+    /**
+     * @param mixed $dateNaissance
+     */
+    public function setDateNaissance($dateNaissance)
+    {
+        $this->dateNaissance = $dateNaissance;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCommentaire()
+    {
+        return $this->commentaire;
+    }
+
+    /**
+     * @param mixed $commentaire
+     */
+    public function setCommentaire($commentaire)
+    {
+        $this->commentaire = $commentaire;
+    }
+
 }
