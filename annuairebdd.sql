d9617e51894f305baf96421e20ab929b1c1141a8
Mariesc <31404263+Mariesc@users.noreply.github.com>
2019-01-20 17:20:33 +0100

    Mise à jour BDD avec donnée test

--- annuairebdd.sql
+++ annuairebdd.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Sam 12 Janvier 2019 à 16:17
+-- Généré le :  Dim 20 Janvier 2019 à 17:19
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
 
@@ -26,7 +26,7 @@ SET time_zone = "+00:00";
 -- Structure de la table `adresse`
 --
 
-CREATE TABLE IF NOT EXISTS `Adresse` (
+CREATE TABLE IF NOT EXISTS `adresse` (
   `A_ID` int(3) unsigned NOT NULL AUTO_INCREMENT,
   `A_NumVoie` int(3) NOT NULL,
   `A_NomVoie` varchar(100) NOT NULL,
@@ -37,13 +37,21 @@ CREATE TABLE IF NOT EXISTS `Adresse` (
   PRIMARY KEY (`A_ID`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
+--
+-- Contenu de la table `adresse`
+--
+
+INSERT INTO `adresse` (`A_ID`, `A_NumVoie`, `A_NomVoie`, `A_ComplementAdresse`, `A_Ville`, `A_CodePostal`, `A_PaysID`) VALUES
+(1, 2, 'Rue du Corocodille', '', 'Bordeaux', '33800', 1),
+(2, 13, 'Avenue Pierre', 'Residence Jean Bat 3 Appt 6', 'Bordeaux', '33800', 2);
+
 -- --------------------------------------------------------
 
 --
 -- Structure de la table `contact`
 --
 
-CREATE TABLE IF NOT EXISTS `Contact` (
+CREATE TABLE IF NOT EXISTS `contact` (
   `C_ID` int(3) unsigned NOT NULL AUTO_INCREMENT,
   `C_Nom` varchar(20) NOT NULL,
   `C_Prenom` varchar(20) NOT NULL,
@@ -52,7 +60,15 @@ CREATE TABLE IF NOT EXISTS `Contact` (
   `C_Societe` varchar(20) DEFAULT NULL,
   `C_Commentaire` varchar(250) DEFAULT NULL,
   PRIMARY KEY (`C_ID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Contenu de la table `contact`
+--
+
+INSERT INTO `contact` (`C_ID`, `C_Nom`, `C_Prenom`, `C_DateNais`, `C_AdresseID`, `C_Societe`, `C_Commentaire`) VALUES
+(1, 'Boby', 'Billy', '2000-12-01', 1, 'Subway', 'je l''aime pas'),
+(2, 'Smith', 'Tara', '1997-03-14', 2, 'Sfr', 'ma soeur');
 
 -- --------------------------------------------------------
 
@@ -60,11 +76,18 @@ CREATE TABLE IF NOT EXISTS `Contact` (
 -- Structure de la table `pays`
 --
 
-CREATE TABLE IF NOT EXISTS `Pays` (
+CREATE TABLE IF NOT EXISTS `pays` (
   `P_ID` int(2) unsigned NOT NULL AUTO_INCREMENT,
   `P_Nom` varchar(50) NOT NULL,
   PRIMARY KEY (`P_ID`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Contenu de la table `pays`
+--
+
+INSERT INTO `pays` (`P_ID`, `P_Nom`) VALUES
+(1, 'France');
 
 -- --------------------------------------------------------
 
@@ -72,12 +95,20 @@ CREATE TABLE IF NOT EXISTS `Pays` (
 -- Structure de la table `telephone`
 --
 
-CREATE TABLE IF NOT EXISTS `Telephone` (
-  `T_numero` int(10) NOT NULL,
+CREATE TABLE IF NOT EXISTS `telephone` (
+  `T_numero` varchar(10) NOT NULL,
   `T_TypeTelID` int(3) NOT NULL,
   `T_ContactID` int(3) NOT NULL,
   PRIMARY KEY (`T_numero`,`T_TypeTelID`,`T_ContactID`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `telephone`
+--
+
+INSERT INTO `telephone` (`T_numero`, `T_TypeTelID`, `T_ContactID`) VALUES
+('0657321309', 2, 2),
+('0664372437', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -85,7 +116,7 @@ CREATE TABLE IF NOT EXISTS `Telephone` (
 -- Structure de la table `type_telephone`
 --
 
-CREATE TABLE IF NOT EXISTS `Type_Telephone` (
+CREATE TABLE IF NOT EXISTS `type_telephone` (
   `TY_ID` int(3) unsigned NOT NULL AUTO_INCREMENT,
   `TY_Portable` tinyint(1) NOT NULL DEFAULT '0',
   `TY_Fixe` tinyint(1) NOT NULL DEFAULT '0',
@@ -95,29 +126,13 @@ CREATE TABLE IF NOT EXISTS `Type_Telephone` (
   PRIMARY KEY (`TY_ID`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
-
 --
--- Structure de la table `typetelephone`
---
-
-DROP TABLE IF EXISTS `typetelephone`;
-CREATE TABLE IF NOT EXISTS `typetelephone` (
-  `Id` int(2) NOT NULL AUTO_INCREMENT,
-  `Nom` varchar(25) NOT NULL,
-  PRIMARY KEY (`Id`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
-
---
--- Déchargement des données de la table `typetelephone`
+-- Contenu de la table `type_telephone`
 --
 
-INSERT INTO `typetelephone` (`Nom`, `Id`) VALUES
-('Fixe', 1),
-('Personnel', 2),
-('Portable', 3),
-('Faxe', 4);
-COMMIT;
-
+INSERT INTO `type_telephone` (`TY_ID`, `TY_Portable`, `TY_Fixe`, `TY_Professionnel`, `TY_Internationnal`, `TY_Fax`) VALUES
+(1, 1, 0, 0, 0, 0),
+(2, 0, 1, 0, 0, 0);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
