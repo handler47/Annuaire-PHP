500b3cfbd6888cf706d2eb2987b639f80190486d
Mariesc <31404263+Mariesc@users.noreply.github.com>
2019-02-03 19:29:17 +0100

    Commentaire class Adresse OK

--- modeles/Adresse.php
+++ modeles/Adresse.php
@@ -1,10 +1,18 @@
 <?php
 /**
-	* Classe Adresse
-*/
+ * Classe Adresse d'un contact
+**/
+
 class Adresse extends Element{
-	//Singleton de mémorisation des instances
+	/**
+	 * Singleton memorise les instances
+	**/
 	private static $o_INSTANCES;
+	
+	/**
+	 * @param $ligne
+	 * renvoie $tmp l'objet créé
+	**/
 	public static function ajouterObjet($ligne){
 		//créer (instancier) la liste si nécessaire
 		if (static::$o_INSTANCES ==null){static::$o_INSTANCES = new Adresses();}
@@ -17,30 +25,44 @@ class Adresse extends Element{
 		return $tmp;
 	}
 	
-	//publication liste instances
+	/**
+	 * @return $o_INSTANCES
+	 * renvoie liste instances
+	**/
 	public static function getInstances(){
-		if (static::$o_INSTANCES ==null){static::$o_INSTANCES = new Adresses();}
+		if (static::$o_INSTANCES ==null){
+			static::$o_INSTANCES = new Adresses();
+		}
 		return static::$o_INSTANCES;
 	}
 		
-	// doit impérativement trouver la Telephone ayant pour id le paramètre
+	/**
+	 * @param $id
+	 * @return Adresse $objet
+	 * doit impérativement trouver l'Adresse ayant pour id le paramètre
+	**/
 	public static function mustFind($id){
 		if (static::$o_INSTANCES == null){static::$o_INSTANCES = new Adresses();}
 		// regarder si instance existe
 		$tmp = static::$o_INSTANCES->getObject($id);
 		if($tmp!=null) {return $tmp;}
-		//sinon pas trouver; chercher dans la BDD
+		//sinon pas trouver -> chercher dans la BDD
 		$req = static::getSELECT().' where A_ID =?';
-		//echo "<br/>recherche $id";
 		$ligne = SI::getSI()->SGBDgetLigne($req, $id);
 		return static::ajouterObjet($ligne);
 	}
 	
-
-	//---------- constructeur : repose sur le constructeur parent
-	protected function __construct($theLigne) {parent::__construct($theLigne);}
+	/**
+	 * constructeur : repose sur le constructeur parent
+	**/
+	protected function __construct($theLigne) {
+		parent::__construct($theLigne);
+	}
 	
-	//---------- renvoie la valeur du champ spécifié en paramètre
+	/**
+	 * @return getField
+	 * renvoie la valeur du champ spécifié en paramètre
+	**/
 	public function getID(){
 		return $this->getField('A_ID');
 	}
@@ -72,6 +94,12 @@ class Adresse extends Element{
 			return null;
 	}
 
+	private $o_pays;
+	
+	/**
+	 * @return o_pays
+	 * renvoie le pays de l'adresse en question
+	**/
 	public function getPays(){
 		if($this->o_pays == null){
 			$this->o_pays = new ListPays();
@@ -79,12 +107,20 @@ class Adresse extends Element{
 		}
 		return $this->o_pays;
 	}
-
+	
+	/**
+	 * @return 
+	 * renvoie les données de l'Adresse
+	**/
 	public function getAdresse(){
 		return $this->getNumVoie() . " " . $this->getNomVoie() . " " . $this->getComplementAdresse() . " "
 			. $this->getCodePostal() . " " . $this->getVille() . " " . $this->getPays()->RechercheNom();
 	}
 
+	/**
+	 * @return 
+	 * renvoie un formulaire avec les attributs de l'Adresse
+	**/
 	public function displayFormulaire(){
 		echo '<label>Numero de la voie : </label>';
         echo '<input type="number" class="champ" name="NumVoie" id="NumVoie" value='.$this->getNumVoie().'>';
@@ -107,12 +143,10 @@ class Adresse extends Element{
         $nomPays = Pays::mustFind($this->getPaysID())->getNom();
         Pays::getInstances()->displaySelect("Pays",$nomPays);
 	}
-
-
-
 	
-	
-	//affiche
+	/** 
+	 * Affiche les attributs de l'Adresse en ligne
+	**/
 	public function displayRow(){
 		echo '<td align="center">'.$this->getNumVoie().'</td>';
 		echo '<td align="center">'.$this->getNomVoie().'</td>';
@@ -121,23 +155,43 @@ class Adresse extends Element{
 	}
 	
 
-	/******************************
-	IMPORTANT : 	toute classe dérivée non abstraite doit avoir le code pour
+	/** 
+	 * @return $id de l'objet Adresse
+	**/
+	public static function champID() {
+		return 'A_ID';
+	}
+	
+	/** 
+	 * @return String $req 
+	 * retourne la requête de la classe Adresse
+	**/
+	public static function getSELECT() {
+		return 'SELECT A_ID,A_NumVoie,A_NomVoie,A_ComplementAdresse,A_Ville,A_CodePostal,A_PaysID FROM adresse';
+	}
 
-	******************************/
-	public static function champID() {return 'A_ID';}
-	public static function getSELECT() {return 'SELECT A_ID,A_NumVoie,A_NomVoie,A_ComplementAdresse,A_Ville,A_CodePostal,A_PaysID FROM adresse';  }
-
+	/** 
+	 * @return $R 
+	 * retourne le message de la requête insertion de la classe Adresse
+	**/
 	public static function SQLInsert(array $valeurs){
 		$req = 'INSERT INTO adresse (A_NumVoie,A_NomVoie,A_ComplementAdresse,A_Ville,A_CodePostal,A_PaysID) VALUES(?,?,?,?,?,?)';
 		return SI::getSI()->SGBDexecuteQuery($req,$valeurs);
 	}
 	
+	/** 
+	 * @return $R
+	 * retourne le message de la requête de suppression de la classe Adresse
+	**/
 	public static function SQLDelete($valeur){
 		$req = 'DELETE FROM adresse WHERE A_ID = ?';
 		return SI::getSI()->SGBDexecuteQuery($req,array($valeur));
 	}
 
+	/** 
+	 * @return $R
+	 * retourne le message de la requête de modification de la classe Adresse
+	**/
 	public static function SQLUpdate(array $valeurs, $condition = null){
 		$req = 'UPDATE Adresse SET A_NumVoie = ?,A_NomVoie = ? ,A_ComplementAdresse = ? ,A_Ville = ? ,A_CodePostal = ? ';
 		if ($condition != null)
@@ -148,13 +202,23 @@ class Adresse extends Element{
 }
 
 
+/**
+ * Classe Adresses
+**/
 class Adresses extends Pluriel{
 
-	//constructeur
+	/**
+	 * constructeur : repose sur le constructeur parent
+	**/
 	public function __construct(){
 		parent::__construct();
 	}
 	
+	/**
+	 * @param String $condition
+	 * @param String $ordre
+	 * Permet la creation d'objet Adresse avec les lignes retournées de la BDD
+	**/
 	public function remplir($condition=null, $ordre=null) {
 		$req = Adresse::getSELECT();
 		//ajouter condition si besoin est
@@ -170,41 +234,47 @@ class Adresses extends Pluriel{
 		}
 	}
 
+	/**
+	 * @param Int $id
+	 * @param String $choix
+	 * Renvoie le pays de l'objet Adresse trouvée
+	**/
 	public function RechercheObjet($id,$choix){
 		if($choix =="pays"){
 			return $this->getObject($id)->getPays();
 		}
 	}
 	
+	/**
+	 * Parcour la liste d'objet Adresse
+	 * Appel un afficheur pour chaque Adresse
+	**/
 	public function displayTable(){
-		// dire à chaque élément de mon tableau : afficher le row
 		foreach ($this->getArray() as $uneadresse) {
 			$uneadresse->displayRow();
 		}
 	}
 	
+	/**
+	 * @return $idAdresse
+	 * Renvoie l'ID des adresses de la liste
+	**/
 	public function displayAdresse(){
 		foreach ($this->getArray() as $uneadresse) {
 			return $uneadresse->getID();
 		}
 	}
 
+	/**
+	 * @return $idAdresse
+	 * Renvoie l'ID des adresses de la liste
+	**/
 	public function displayAdresseObject(){
 		foreach ($this->getArray() as $uneadresse) {
 			return $uneadresse;
 		}
 	}
 
-
-	public function displaySelect($name){
-		echo'<select style="width:auto" class="form-control" type="Text" required="required" name="'.$name.'">';
-		echo '<option>  </option>';
-		// dire à chaque élément de mon tableau : afficher le row
-		foreach ($this->getArray() as $uneadresse) {
-			$uneadresse->option();
-		}
-		echo '</select>';
-	}
 	
 }
 ?>
